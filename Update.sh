eba8addaf790226f3e470f6178fb86789e015b95
Jakub Nowak <jakub.jakub.nowak@gmail.com>
2018-12-28 01:24:54 +0100

    Add update script

---
+++ Update.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+wget https://raw.githubusercontent.com/MrQubo/CookieMonster/master/CookieMonster.js -O CookieMonster.js
+wget https://raw.githubusercontent.com/MrQubo/CookieMonster/beta/CookieMonster.js -O CookieMonsterBeta.js
